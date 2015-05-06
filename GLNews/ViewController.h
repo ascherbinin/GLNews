e4ed9914565d44d36e0db295925381b542233872
Admin <admin@Mac-Admin.local>
2015-05-06 05:15:53 +0300

    Initial Commit

---
+++ GLNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GLNews
+//
+//  Created by Admin on 06.05.15.
+//  Copyright (c) 2015 GoodLine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
